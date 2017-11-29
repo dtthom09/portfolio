699bd75dd8a5a16f604ba671e402185b830c6dab
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2017-11-28 20:55:45 -0500

    Web Dev folder update

---
+++ Web Development/Program4/Program4/Prog1A/DescendingByZip.cs
@@ -0,0 +1,35 @@
+﻿//Prog4
+//Grading ID: C1945
+//Due date: 11-29-16
+//CIS200-01
+//This program explores ways of comparing objects in a class by using ICombarable
+//and sorting these objects in different orders.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    public class DescendingByZip : Comparer<Parcel>
+    {
+        //Precondition:  None
+        //Postcondition: Overrides compare and orders parcels by desc zip
+        //               When parcel1 < parcel2, method returns positive #
+        //               When parcel1 == parcel2, method returns zero
+        //               When parcel1 > parcel2, method returns negative #
+        public override int Compare(Parcel parcel1, Parcel parcel2)
+        {
+            //Ensure correct handling of null values (in .NET, null less than anything)
+            if (parcel1 == null && parcel2 == null) //Checks if both are null
+                return 0;                           //Equal
+            if (parcel1 == null)                    //Checks if parcel1 is null
+                return -1;                          //Null is less than any actual parcel
+            if (parcel2 == null)                    //Checks if parcel2 is null
+                return 1;                           //Any actual parcel is greater than null
+
+            //Reverses natural order, so descending
+            return (-1)*(parcel1.DestinationAddress.Zip).CompareTo(parcel2.DestinationAddress.Zip);
+        }
+    }
+}
---
+++ Web Development/Program4/Program4/Prog1A/Parcel.cs
@@ -0,0 +1,80 @@
+﻿// Program 0
+// CIS 200-01/76
+// Fall 2016
+// Due: 9/24/2016
+// By: Andrew L. Wright (students use Grading ID)
+
+// File: Parcel.cs
+// Parcel serves as the abstract base class of the Parcel hierachy.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class Parcel : IComparable<Parcel>
+{
+    // Precondition:  None
+    // Postcondition: The parcel is created with the specified values for
+    //                origin address and destination address
+    public Parcel(Address originAddress, Address destAddress)
+    {
+        OriginAddress = originAddress;
+        DestinationAddress = destAddress;
+    }
+
+    public Address OriginAddress
+    {
+        // Precondition:  None
+        // Postcondition: The parcel's origin address has been returned
+        get;
+
+        // Precondition:  None
+        // Postcondition: The parcel's origin address has been set to the
+        //                specified value
+        set;
+    }
+
+    public Address DestinationAddress
+    {
+        // Precondition:  None
+        // Postcondition: The parcel's destination address has been returned
+        get;
+
+        // Precondition:  None
+        // Postcondition: The parcel's destination address has been set to the
+        //                specified value
+        set;
+    }
+
+    // Precondition:  None
+    // Postcondition: The parcel's cost has been returned
+    public abstract decimal CalcCost();
+
+    // Precondition:  None
+    // Postcondition: A String with the parcel's data has been returned
+    public override String ToString()
+    {
+        return String.Format("Origin Address:{3}{0}{3}{3}Destination Address:{3}{1}{3}Cost: {2:C}",
+            OriginAddress, DestinationAddress, CalcCost(), Environment.NewLine);
+    }
+
+    //Precondition:  None
+    //Postcondition: When this < parcel2, method returns negative #
+    //               When this == parcel2, method returns zero
+    //               When this > parcel2, method returns positive #
+    public int CompareTo(Parcel parcel2)
+    {
+        // Ensure correct handling of null values (in .NET, null less than anything)
+
+        if (this == null && parcel2 == null)    //Checks if both are null
+            return 0;                           //Equal
+        if (this == null)                       //Checks if this is null
+            return -1;                          //Null is less than any actual parcel
+        if (parcel2 == null)                    //Checks if parcel2 is null
+            return 1;                           //Any actual parcel is greater than null
+
+        //Compare by calc cost
+        return (this.CalcCost()).CompareTo(parcel2.CalcCost());
+    }
+}
